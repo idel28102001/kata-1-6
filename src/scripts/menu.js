261b742056428b5b71ad523e3566f4b6eadfd692
Idel Gataullin <idel28102001@mail.ru>
2023-02-13 08:38:29 +0300

    process

---
+++ src/scripts/menu.js
@@ -0,0 +1,11 @@
+
+const menu = document.querySelector('.mobile-menu');
+const burgerOpen = document.querySelector('.header__burger');
+const burgerClose = document.querySelector('.mobile-header__button');
+
+burgerOpen.addEventListener('click', () => {
+    menu.classList.add('mobile-menu--active');
+});
+burgerClose.addEventListener('click', () => {
+    menu.classList.remove('mobile-menu--active');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,26 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all",
+        },
+        minimizer: [],
+    }
+    if (isProd) {
+        const plugins = [
+            new CssMinimizerPlugin(),
+            new TerserWebpackPlugin(),
+        ];
+        config.minimizer.push(...plugins)
+    }
+    return config;
+};
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 const cssLoaders = extra => {
     const loaders = [MiniCssExtractPlugin.loader, 'css-loader'];
     if (extra) loaders.push(extra);
@@ -19,12 +37,18 @@ module.exports = {
     entry: {
         main: './index.js',
     },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[name][ext][query]'
+    },
     resolve: {
-        extensions: ['.js', '.json'],
+        extensions: ['.js', '.json', '.css', '.scss'],
         alias: {
             "@": path.resolve(__dirname, 'src'),
         },
     },
+    optimization: optimization(),
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html',
@@ -33,8 +57,22 @@ module.exports = {
             }
         }),
         new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin(
+            {
+                filename: filename('css')
+            }
+        ),
+        new CssMinimizerPlugin(),
     ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'src'),
+        },
+        compress: true,
+        port: 4200,
+        open: true,
+        hot: isDev,
+    },
     module: {
         rules: [
             {
@@ -49,6 +87,10 @@ module.exports = {
                 test: /\.(png|jpeg|jpg|svg|gif)$/,
                 type: 'asset/resource',
             },
+            {
+                test: /\.html/,
+                use: ['html-loader'],
+            }
         ],
     },
 }
