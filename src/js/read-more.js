ff3919fca658460372ff6008607ab3f135665ccd
Idel Gataullin <idel28102001@mail.ru>
2023-02-23 22:43:06 +0300

    Хорошо сделал. Осталось менюшку улучшить

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,5 +1,19 @@
+
 const description = document.querySelector('.information__description');
 const button = document.querySelector('.information__button');
+const classText = 'information__description--exposed';
+const func = () => {
+    const wholeHeight = description.scrollHeight - 5;
+    const visibleHeight = description.offsetHeight;
+    if (wholeHeight <= visibleHeight && !description.classList.contains(classText)) {
+        button.style.display = 'none';
+    } else {
+        button.style.display = 'flex';
+    }
+}
 button.addEventListener('click', () => {
-    description.classList.toggle('information__description--exposed');
+    description.classList.toggle(classText);
+    func();
 });
+func()
+window.addEventListener('resize', func);
